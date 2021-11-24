dc7ec8a1cccff9edf5c2f57059e7f858dcc8abf5
william jimenez <>
2021-11-23 21:55:47 -0500

    first commit

---
+++ src/main/java/com/wheels2/travel_ms/TravelMsApplication.java
@@ -0,0 +1,13 @@
+package com.wheels2.travel_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheels2/travel_ms/TravelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheels2.travel_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
