06931e5fa711a12fde22cd97c8593071b48d9813
Bayron David <93720978+BayronDavid@users.noreply.github.com>
2021-11-28 12:33:25 -0500

    Revert "Creacion de Modelo Travel, TravelRepository , TravelNotFoundAdvice y …"

--- src/main/java/com/wheels2/travel_ms/exceptions/TravelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.wheels2.travel_ms.exceptions;
-
-public class TravelNotFoundException extends RuntimeException{
-    public TravelNotFoundException(String message) {
-        super(message);
-    }
-}
